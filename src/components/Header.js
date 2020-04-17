07f8e4ee0f4316fb97b6af0c3b963ba3fb527e9c
andresfelipevasquez <andresfelipevasquez@live.com>
2020-04-17 03:00:23 -0500

    Add styles Header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,18 +2,34 @@ import React from 'react';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 const Header = () => {
     return(
         <div>
-            <AppBar position="static" style={{ background: '#2E3B55' }}>
+            <AppBar position="static" style={{ background: '#010915' }}>
             <Toolbar>
-                <Typography variant="h6">
+                <Typography variant="h6" style={{ flex: 1 }}>
                     COVID-19 en Colombia
                 </Typography>                
+                <Button 
+                    color="inherit"
+                    style={{ textTransform: 'none' }}
+                    target="_blank"
+                    href={"https://www.datos.gov.co/Salud-y-Protecci-n-Social/Casos-positivos-de-COVID-19-en-Colombia/gt2j-8ykr"}
+                 >Ir a fuente de datos
+                 </Button>
+                <Button 
+                    color="inherit"
+                    target="_blank"
+                    href={"https://github.com/andresfelipevasquez/covid-colombia.git"}
+                >
+                    <GitHubIcon></GitHubIcon>
+                </Button>
             </Toolbar>
             </AppBar>
         </div>
-);
+    );
 }
 export default Header;
